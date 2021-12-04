7d415422579951f48a1ac3504c173c3a3f0775b2
Sahana Kolla <95235784+SahanaKolla@users.noreply.github.com>
2021-12-04 13:48:55 +0530

    Delete maths.py

--- maths.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter first value")
-b=input("Enter second value")
-print("Addition:",a+b)
-print("Substraction:",a-b)
-print("Multiplication:",a*b)
-print("Division:",a/b)
-print("Reminder:",a%b)
-
-
