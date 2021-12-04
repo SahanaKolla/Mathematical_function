523fc66084e2ce8a8acf0ee0d772e27bb2e8985e
Sahana Kolla <95235784+SahanaKolla@users.noreply.github.com>
2021-12-04 06:51:23 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,9 @@
+a=input("Enter first value")
+b=input("Enter second value")
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Reminder:",a%b)
+
+
