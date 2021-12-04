6eeef6a1fc0c673af744f3f50e45a5c104e80a27
Sahana Kolla <95235784+SahanaKolla@users.noreply.github.com>
2021-12-04 06:50:59 +0530

    Add files via upload

---
+++ math.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+int a,b;
+printf("Enter two numbers");
+scanf("%d %d",&a,&b);
+  printf("Addition of %d,%d is %d\n", a,b,a+b);
+  printf("Substraction of %d,%d is %d\n", a,b,a-b);
+  printf("Multiplication of %d,%d is %d\n", a,b,a*b);
+  printf("Division of %d,%d is %d\n", a,b,a/b);
+  printf("Reminder of %d,%d is %d\n", a,b,a%b);
+}
